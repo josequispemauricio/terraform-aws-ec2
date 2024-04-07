181d576bf99a6a8ecffd5e9dfcae5d199ec8ceef
josegabrielquispemauricio <jose.quispe@external.totalenergies.fr>
2024-04-07 19:13:58 +0200

    updateToTestInfracost

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 
 
 
-resource "aws_instance" "vm" {
+resource "aws_instance" "vm1" {
+  ami="ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
+  subnet_id = var.subnet_id
+}
+
+
+resource "aws_instance" "vm2" {
   ami="ami-0c101f26f147fa7fd"
   instance_type = "t2.micro"
   subnet_id = var.subnet_id
